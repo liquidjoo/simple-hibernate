e18ea6c731f64a38018589a6d017200b93572182
liquidjoo <liquidjoo@gmail.com>
2021-08-26 11:13:51 +0900

    feat : reflection 을 사용해 Car 필드값 가져오기

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -3,9 +3,14 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class CarTest {
 
@@ -18,7 +23,15 @@ class CarTest {
 
     @Test
     @DisplayName("reflection 을 사용해 Car 필드값 가져오기")
-    void name() {
+    void getFields() {
+        Class<Car> carClass = Car.class;
+        Field[] declaredFields = carClass.getDeclaredFields();
 
+        assertAll("car fields", () -> {
+           assertThat(declaredFields[0].getName()).isEqualTo("id");
+           assertThat(declaredFields[1].getName()).isEqualTo("name");
+           assertThat(declaredFields[2].getName()).isEqualTo("price");
+           assertThat(declaredFields[3].getName()).isEqualTo("releaseDate");
+        });
     }
 }
